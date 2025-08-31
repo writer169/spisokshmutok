17c41d5f5a20ce9f1cfc8e07057c601609390c94
writer169 <148839750+writer169@users.noreply.github.com>
2025-08-31 18:24:16 +0500

    Create pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster 
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
+    </>
+  );
+}
